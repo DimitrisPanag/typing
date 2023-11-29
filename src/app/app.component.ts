ce4dd339cf9339d8a2b25bf8a8b75ded4d32f482
Dimitris Panagiotou <jim.panag2000@gmail.com>
2023-11-29 20:00:18 +0200

    Made the app lighter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CardComponent } from './card/card.component';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,CardComponent],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+
+  randomText = faker.lorem.sentence();
+  textLength ='';
+
+  onInputInteract(target: EventTarget){
+    const value = (<HTMLInputElement>target).value;
+    this.textLength = value;
+  }
+
+  onComparison(randomLetter: string, enteredLetter: string){
+    if(!enteredLetter){
+      return 'pending';
+    }
+    return enteredLetter === randomLetter ? 'correct' : 'incorrect';
+  }
   
 }
